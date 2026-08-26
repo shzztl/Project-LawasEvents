2f2b059da0b636f92bb4f367b2f2ac24534da60d
shzztl <138546325+shzztl@users.noreply.github.com>
2026-08-26 16:53:43 +0800

    Implement CalendarActivity to display calendar fragment

---
+++ CalendarActivity.java
@@ -0,0 +1,35 @@
+package com.example.lawaseventia;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_calendar);
+
+        String calendarDate = getIntent().getStringExtra("calendarDate");
+
+        CalendarFragment fragment = new CalendarFragment();
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString(
+                "calendarDate",
+                calendarDate
+        );
+
+        fragment.setArguments(bundle);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(
+                        R.id.calendarContainer,
+                        fragment
+                )
+                .commit();
+    }
+}
