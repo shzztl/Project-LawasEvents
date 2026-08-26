1d2e9a53ec31ffeb7ade412283fdd1a8d57d6dc4
shzztl <138546325+shzztl@users.noreply.github.com>
2026-08-26 14:52:09 +0800

    Rename package from lawasevents to lawaseventia

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,8 @@
-package com.example.lawasevents;
+package com.example.lawaseventia;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
