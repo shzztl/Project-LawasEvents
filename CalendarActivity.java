837b9e394457a3ad2843ba5c533d2c30f7a04f11
shzztl <138546325+shzztl@users.noreply.github.com>
2026-09-02 11:35:07 +0800

    Pass FROM_EVENT_DETAILS to CalendarFragment

--- CalendarActivity.java
+++ CalendarActivity.java
@@ -4,22 +4,25 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalendarActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_calendar);
 
         String calendarDate = getIntent().getStringExtra("calendarDate");
+        boolean fromEventDetails =getIntent().getBooleanExtra("FROM_EVENT_DETAILS", false);
 
         CalendarFragment fragment = new CalendarFragment();
-
         Bundle bundle = new Bundle();
 
         bundle.putString(
                 "calendarDate",
                 calendarDate
+        );
+
+        bundle.putBoolean(
+                "FROM_EVENT_DETAILS",
+                fromEventDetails
         );
 
         fragment.setArguments(bundle);
